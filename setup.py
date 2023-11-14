e15bf6f308112f9beb21a4618f13ba4bf9117fdd
Marko2155 <welsheyjack@gmail.com>
2023-11-14 21:08:55 +0100

    Added stuff to upload it to PyPI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="libBuffer",
+    description="Library to help with handling files in memory.",
+    version="1.0",
+    packages=find_packages(),
+)
