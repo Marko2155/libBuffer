a967da893db2940f58cb12046252be8301d40833
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2023-11-14 21:17:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="libBuffer",
-    description="Library to help with handling files in memory.",
-    version="1.0",
-    packages=find_packages(),
+    name='libBuffer',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    keywords="buffer",
+    version='1.0.3',
+    packages=['tbgdl'],
+    install_requires=[],
+    url='https://github.com/Marko2155/libBuffer',
+    license='MIT',
+    author='Marko Camandioti',
+    author_email='camandiotimarko@gmail.com',
+    description="Library for Python to help with storing shit in memory (will reset after user has closed program)",
+    project_urls={
+        'Documentation': 'https://github.com/Marko2155/libBuffer#readme',
+        'Source': 'https://github.com/Marko2155/libBuffer',
+        'Tracker': 'https://github.com/Marko2155/libBuffer/issues',
+    }
 )
